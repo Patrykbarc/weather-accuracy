771bcae6ff98065f01a7fe6fc542d79c69d34003
Patryk Barć <108985559+Patrykbarc@users.noreply.github.com>
2026-08-14 22:43:40 +0200

    add React integration and OpenAPI type generation

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,9 +3,18 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      proxy: {
+        "/api": "http://localhost:8000",
+      },
+    },
   },
+
+  integrations: [react()],
 });
